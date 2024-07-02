0879d94cd85e25569ebbe8babe0dc27719e00792
abtaheetaseen <abtahee.siddiqui@northsouth.edu>
2024-07-03 00:46:21 +0600

    second commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,6 +12,16 @@ const About = () => {
 
         <div>
             <p>As a react developer, I am well-versed in numerous computing scripts and libraries, including JavaScript, React, HTML, CSS, and Tailwind CSS. Additionally, I am conversant with server-side scripts like Rest API, MongoDB, Node.js, and Express.js. My GitHub account and projects demonstrate my coding skills. My expertise ranges from developing responsive and adaptable designs and functionality to enhancing website performance for a smooth user experience.</p>
+
+            <div className='mt-5'>
+                <h1 className='text-xl font-semibold'>Education</h1>
+                <div>
+                    <h1 className='text-md'>North South University</h1>
+                    <p className='text-sm'>Computer Science and Engineering</p>
+                    <p className='text-xs'>Jan 2021 - Present</p>
+                </div>
+            </div>
+
             <div className='flex items-center justify-center mt-10'>
                 <a className='btn btn-sm border-none bg-yellow-500 hover:bg-yellow-400 text-white' target='_blank' alt="my resume" rel='noopener noreferrer' href='./ABTAHEE ASHAB SIDDIQUI-RESUME(react developer).pdf'>
                     Download Resume
